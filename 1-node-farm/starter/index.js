37f9e856a5a5cdc9768ae4e4902a77a732fcc81b
Elangodevguy <elangodevguy@gmail.com>
2023-04-24 10:33:56 +0530

    8_reading and writinmg files

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,6 @@
-const hello = "Hello world";
 const fs = require("fs");
 
-console.log(fs);
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
