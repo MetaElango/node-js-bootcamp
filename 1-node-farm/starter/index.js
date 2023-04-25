5496edcdc5b34c3d37426d264caace917f46e3fe
Elangodevguy <elangodevguy@gmail.com>
2023-04-25 11:07:45 +0530

    10_reading_and_writing_files

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,18 @@
 const fs = require("fs");
+// Synchronous and blocking mode
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
-const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+
+// Async and non blocking mode
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data2) => {
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      fs.writeFile("./txt/output.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log(err);
+      });
+    });
+  });
+});
