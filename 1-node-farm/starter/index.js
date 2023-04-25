65b784b445b5179bfc49685e4e37c1e7b8c4b80a
Elangodevguy <elangodevguy@gmail.com>
2023-04-25 11:42:28 +0530

    11_creating_a_simple_web_server

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const http = require("http");
 // Synchronous and blocking mode
 
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -7,12 +8,23 @@ const fs = require("fs");
 // fs.writeFileSync("./txt/output.txt", textOut);
 
 // Async and non blocking mode
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-  fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data2) => {
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      fs.writeFile("./txt/output.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log(err);
-      });
-    });
-  });
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//   fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data2) => {
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       fs.writeFile("./txt/output.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log(err);
+//       });
+//     });
+//   });
+// });
+
+// //////////////////////////////////////////////////////////////////
+// Create server
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server");
+});
+
+server.listen("8000", "127.0.0.1", () => {
+  console.log("Listening to request on port : 8000");
 });
