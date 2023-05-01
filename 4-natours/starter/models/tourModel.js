7662aceb7e427227144a3c0c95b0953b2b2caad5
Elangodevguy <elangodevguy@gmail.com>
2023-05-01 17:07:45 +0530

    93_modelling the tours

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -5,14 +5,53 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a maximum group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a Difficulty details'],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A Tour must have a price'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a image cover'],
+  },
+  images: {
+    type: [String],
+  },
+  startDates: [Date],
+  createAt: {
+    type: Date,
+    default: Date.now(),
   },
 });
 
