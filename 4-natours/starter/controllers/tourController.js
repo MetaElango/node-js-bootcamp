f9ff8d53ea362d3f23aa45e5463aef8733987ed6
Elangodevguy <elangodevguy@gmail.com>
2023-05-01 14:54:59 +0530

    89 another way of creating document

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,3 +1,5 @@
+const Tour = require('../models/tourModel');
+
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
@@ -12,12 +14,21 @@ exports.getAllTours = (req, res) => {};
 
 exports.getTour = (req, res) => {};
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: { tour: newTour },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    return res.status(201).json({
+      status: 'success',
+      data: { tour: newTour },
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Invalid input',
+    });
+  }
 };
+
 exports.updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
