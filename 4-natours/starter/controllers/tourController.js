8be7e90c3fbb377ab87511475e7c452728f16d9d
Elangodevguy <elangodevguy@gmail.com>
2023-05-01 15:42:23 +0530

    91_updating documents

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,18 +1,40 @@
 const Tour = require('../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    return res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Missing name or price',
+      message: 'Invalid input',
     });
   }
-  next();
 };
 
-exports.getAllTours = (req, res) => {};
-
-exports.getTour = (req, res) => {};
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    return res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
 
 exports.createTour = async (req, res) => {
   try {
@@ -29,13 +51,25 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: `<Updated tour here>`,
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    return res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 exports.deleteTour = (req, res) =>
