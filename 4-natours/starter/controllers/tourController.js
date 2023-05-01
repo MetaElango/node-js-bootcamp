2262852948fd1c913058cff4dcf7654f72448d5f
Elangodevguy <elangodevguy@gmail.com>
2023-05-01 15:48:12 +0530

    92_deleting documents

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -72,8 +72,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) =>
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    return res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
