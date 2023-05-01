dc3846d79f4f221561f9f0d70c4780682f5a45ec
Elangodevguy <elangodevguy@gmail.com>
2023-05-01 12:47:02 +0530

    83_connecting our db with express app

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,18 @@
+const mongoose = require('mongoose');
 require('dotenv').config({ path: './config.env' });
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace('<password>', process.env.PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection established'));
 // Start server
 const port = 3000;
 app.listen(port, () => {
