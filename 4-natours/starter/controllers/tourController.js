a5ccdc612c03b7998c736e71573ee68d4e5a7fbe
Elangodevguy <elangodevguy@gmail.com>
2023-05-02 13:25:33 +0530

    95_ making the api better: filtering

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,7 +2,14 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // build query
+    const query = Tour.find(queryObj);
+    // Execute Query
+    const tours = await query;
 
     return res.status(200).json({
       status: 'success',
