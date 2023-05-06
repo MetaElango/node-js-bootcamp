bff93e9ec6a4fa98a8801252ce1ab47540db15cb
Elangodevguy <elangodevguy@gmail.com>
2023-05-06 13:55:27 +0530

    112 handling unhandeled routes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -18,8 +18,14 @@ app.use((req, res, next) => {
 });
 
 // Routes
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
