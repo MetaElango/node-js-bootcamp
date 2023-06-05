9a18167ee963e583653196686f4275c9a071c41c
Elangodevguy <elangodevguy@gmail.com>
2023-06-05 11:42:33 +0530

    added body parser

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -30,6 +30,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // body parser reading data from body
+app.use(express.json({ limit: '10kb' }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
