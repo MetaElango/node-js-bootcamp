2dc9fe7fe7b0506c1fc8300c02b8df74094dde19
Elangodevguy <elangodevguy@gmail.com>
2023-06-12 12:33:29 +0530

    151 modellinng tour guide embedding

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { default: slugify } = require('slugify');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -99,6 +100,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -107,6 +109,11 @@ const tourSchema = new mongoose.Schema(
 );
 tourSchema.pre('save', function (next) {
   this.slugify = slugify(this.name, { lower: true });
+  next();
+});
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 // tourSchema.pre('save', function (next) {
